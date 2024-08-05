7f8a72e7fb45dcc728754cf3d1b421bc3507c406
zack781 <thean781@gmail.com>
2024-08-04 22:43:41 -0400

    Added README.md and Created SwiftUI Project

---
+++ glasspp/glasspp/glassppApp.swift
@@ -0,0 +1,17 @@
+//
+//  glassppApp.swift
+//  glasspp
+//
+//  Created by Zack Nguyen on 8/4/24.
+//
+
+import SwiftUI
+
+@main
+struct glassppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
